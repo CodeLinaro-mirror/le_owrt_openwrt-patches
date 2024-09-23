5c19695fba1e8186971a29632206cc95caee9847
Bulbul Dabi <quic_bdabi@quicinc.com>
2024-09-24 00:15:43 +0530

    Add dependency on pcre2
    pcre2 is used instead of pcre in latest version
    
    Change-Id: Ib009de2901bee5e9518e4e127db73bcaa2e6156a

---
+++ package/feeds/qtibsp/ext4-utils/ext4_utils.mk
@@ -0,0 +1,7 @@
+define Package/ext4_utils
+        SECTION:=QTI
+        CATEGORY:=QTI-PACKAGES
+        TITLE:= ext4_utils
+        DEPENDS:= +libselinux +libsparse +libcutils +libpcre2 +core-include
+endef
+
