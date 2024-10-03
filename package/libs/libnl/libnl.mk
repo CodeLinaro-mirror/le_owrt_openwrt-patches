6dae7ef24906f3b22698af04c8bc32d05343ae3a
Challa Koti Reddy <quic_challako@quicinc.com>
2024-10-03 14:38:02 +0530

    libnl: Compilation fix
    Replaces 8 spaces with a tab to fix compilation issue.
    
    Change-Id: I4d6a7c30f85f271630fb8d501bb5ed6658a82a3d

--- package/libs/libnl/libnl.mk
+++ package/libs/libnl/libnl.mk
@@ -1,6 +1,6 @@
 define libnl_dev_append
-        mkdir -p $(1)/usr/include/libnl
-        $(CP) $(PKG_BUILD_DIR)/include/* $(1)/usr/include/libnl/
+	mkdir -p $(1)/usr/include/libnl
+	$(CP) $(PKG_BUILD_DIR)/include/* $(1)/usr/include/libnl/
 endef
 
 Build/InstallDev += $(newline)$(libnl_dev_append)
