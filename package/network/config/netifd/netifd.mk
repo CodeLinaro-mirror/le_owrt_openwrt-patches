0222bfcdc5cd077aebdcb38f8e64bdb9bb193c1d
HuaWen CHEN <quic_huawenc@quicinc.com>
2024-12-18 15:29:31 +0800

    netifd/odhcp6c: patch for backhaul switch
    Ensure the file permissive +x.
    
    Change-Id: Id4dfd58e40655e323ab410aa762b2f16844164b1
    Signed-off-by: HuaWen CHEN <quic_huawenc@quicinc.com>

--- package/network/config/netifd/netifd.mk
+++ package/network/config/netifd/netifd.mk
@@ -1,4 +1,7 @@
+PATCH_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 define netifd_append
+	$(CP) $(PATCH_DIR)/dhcp.script $(1)/lib/netifd/dhcp.script
+	$(CP) $(PATCH_DIR)/dhcp.sh $(1)/lib/netifd/proto/dhcp.sh
 	chmod 0644 $(1)/lib/netifd/netifd-proto.sh
 endef
 
