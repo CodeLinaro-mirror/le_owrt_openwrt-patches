f1195458aca8106a6d4cdc4e7a36b3f0d09ebec8
HuaWen CHEN <quic_huawenc@quicinc.com>
2025-03-10 22:47:29 +0800

    wifi-script: remove netifd script
    Change-Id: Ie47dd52b912bb74ca95b4b610b22c79ea8bccb1f

--- package/network/config/wifi-scripts/wifi-scripts.mk
+++ package/network/config/wifi-scripts/wifi-scripts.mk
@@ -3,6 +3,7 @@ WIFI_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 define wifi_install_append
 	$(INSTALL_DIR) $(1)/sbin
 	$(CP) $(WIFI_DIR)/files/sbin/wifi $(1)/sbin/wifi
+	rm -rf $(1)/lib/netifd/wireless
 endef
 
 Package/wifi-scripts/install += $(newline)$(wifi_install_append)
