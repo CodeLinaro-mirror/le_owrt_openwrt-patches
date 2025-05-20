c82e7a4c98de66c25a77a4a1069fcaf5dcd0d665
Utkrisht Sikka <usikka@qti.qualcomm.com>
2025-05-20 12:36:42 +0530

    dnsmasq - When the system time changes on bootup, dnsmasq deletes entry with old timestamp. So, use broken rtc flag
    Change-Id: I4d0dde1107489d7e84413359186f953f45421b93

--- package/network/services/dnsmasq/dnsmasq.mk
+++ package/network/services/dnsmasq/dnsmasq.mk
@@ -1,4 +1,5 @@
 DNSMASQ_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+MAKE_FLAGS += CFLAGS="$(TARGET_CFLAGS) -DHAVE_BROKEN_RTC"
 define dnsmasq_append
 	$(INSTALL_DIR) $(1)/etc
 	$(CP) $(DNSMASQ_DIR)/files/dnsmasq.conf $(1)/etc/dnsmasq.conf
