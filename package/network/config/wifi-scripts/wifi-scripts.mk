08d4f91f481a32059f2ef5b5f49bd808927c9a96
HuaWen CHEN <huawenc@qti.qualcomm.com>
2025-07-23 09:35:28 +0800

    wifi-script: remove hotplug script for ieee80211
    Change-Id: Idc31245c07455d3b546408f824dddb648724cc63

--- package/network/config/wifi-scripts/wifi-scripts.mk
+++ package/network/config/wifi-scripts/wifi-scripts.mk
@@ -4,6 +4,7 @@ define wifi_install_append
 	$(INSTALL_DIR) $(1)/sbin
 	$(CP) $(WIFI_DIR)/files/sbin/wifi $(1)/sbin/wifi
 	rm -rf $(1)/lib/netifd/wireless
+	rm -rf $(1)/etc/hotplug.d/ieee80211/10-wifi-detect
 endef
 
 Package/wifi-scripts/install += $(newline)$(wifi_install_append)
