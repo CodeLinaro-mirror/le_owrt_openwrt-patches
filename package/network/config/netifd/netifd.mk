6d2c7b542f7003fc4652d816e6e7954826049c6e
Prakhar Vyas <pvyas@qti.qualcomm.com>
2025-08-25 11:33:21 +0530

    netifd: avoid invoking packet-steering.uc script on network reload
    Change-Id: I69178142a9fe4f50172aa04fc19568dd5423309b
    (cherry picked from commit b0a9dc6390ddd34068f4ee4e151b68144f8b66af)

--- package/network/config/netifd/netifd.mk
+++ package/network/config/netifd/netifd.mk
@@ -2,6 +2,7 @@ PATCH_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 define netifd_append
 	$(CP) $(PATCH_DIR)/dhcp.script $(1)/lib/netifd/dhcp.script
 	$(CP) $(PATCH_DIR)/dhcp.sh $(1)/lib/netifd/proto/dhcp.sh
+	$(CP) $(PATCH_DIR)/packet_steering $(1)/etc/init.d/packet_steering
 	chmod 0644 $(1)/lib/netifd/netifd-proto.sh
 endef
 
