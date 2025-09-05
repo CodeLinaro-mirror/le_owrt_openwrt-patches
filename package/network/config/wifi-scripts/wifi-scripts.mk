6a786f8c9de1d1817bb1a8afe65383bed5a510e0
BabuSubashChandar C <babusuba@qti.qualcomm.com>
2025-09-05 22:05:25 +0530

    wifi-scripts install append patched to match IPQ steps.
    Change-Id: Iab3f7f4e179b892d759fac6a786d1edc9ac3390f
    Signed-off-by: BabuSubashChandar C <babusuba@qti.qualcomm.com>

--- package/network/config/wifi-scripts/wifi-scripts.mk
+++ package/network/config/wifi-scripts/wifi-scripts.mk
@@ -1,10 +1,13 @@
 WIFI_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
+
+ifeq ($(CONFIG_PACKAGE_kmod-ath11k)$(CONFIG_PACKAGE_kmod-ath12k),)
 define wifi_install_append
 	$(INSTALL_DIR) $(1)/sbin
 	$(CP) $(WIFI_DIR)/files/sbin/wifi $(1)/sbin/wifi
 	rm -rf $(1)/lib/netifd/wireless
 	rm -rf $(1)/etc/hotplug.d/ieee80211/10-wifi-detect
 endef
+endif
 
 Package/wifi-scripts/install += $(newline)$(wifi_install_append)
