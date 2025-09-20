f36192a2a79e4975777ae30c7e060fe31bc223a5
divypate <divypate@qti.qualcomm.com>
2025-09-20 14:28:11 +0530

    Revert "Fix for pbr rule causing extra delay in firewall reload"
    This reverts commit 7ea79a91c24f6b9c0ae199c1fe15469aa628e4b2.
    
    Change-Id: Ifcb009f6a80d62ef1e508f55f0828591229048ed

--- package/feeds/packages/pbr/pbr.mk
+++ package/feeds/packages/pbr/pbr.mk
@@ -5,8 +5,6 @@ define pbr_append
 	$(INSTALL_DATA) $(PBR_DIR)/files/etc/hotplug.d/iface/71-pbr $(1)/etc/hotplug.d/iface/71-pbr
 	$(INSTALL_BIN) $(PBR_DIR)/files/etc/init.d/pbr_patched $(1)/etc/init.d/pbr
 	$(INSTALL_DATA) $(PBR_DIR)/files/etc/firewall.user.pbr $(1)/etc/firewall.user.pbr
-	$(INSTALL_DIR) $(1)/etc/uci-defaults
-	$(INSTALL_BIN) $(PBR_DIR)/files/etc/uci-defaults/90-pbr $(1)/etc/uci-defaults/90-pbr
 endef
 
 Package/pbr/default/install += $(newline)$(pbr_append)
